e1bb03997a3ffafc8efe8c5b204a47a9bde2ec7c
Svein Gunnar Larsen <Svein Gunnar Larsen>
2016-08-05 09:17:09 +0200

    Initial commit

---
+++ src/ios/LineaProCDV.h
@@ -0,0 +1,26 @@
+//
+//  LineaProCDV.h
+//
+//  Created by Timofey Tatarinov on 27.01.14.
+//  Citronium
+//  http://citronium.com
+//
+
+#import <UIKit/UIKit.h>
+#import <Cordova/CDVPlugin.h>
+
+#import "DTDevices.h"
+
+@interface LineaProCDV : CDVPlugin
+{
+    DTDevices *dtdev;
+}
+
+- (void)initDT:(CDVInvokedUrlCommand*)command;
+- (void)getConnectionStatus:(CDVInvokedUrlCommand*)command;
+- (void)startBarcode:(CDVInvokedUrlCommand*)command;
+- (void)stopBarcode:(CDVInvokedUrlCommand*)command;
+- (void)startRFID:(CDVInvokedUrlCommand*)command;
+- (void)closeRFID:(CDVInvokedUrlCommand*)command;
+
+@end
